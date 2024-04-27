487eabea7b9789d9fa2325aaf9baf8f224eb70ee
Dingyang-Zuo-1158919 <Dingyang.Zuo@lincolnuni.ac.nz>
2024-04-27 20:44:17 +1200

    pic added

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -15,7 +15,7 @@ namespace API.Data
                     Name = "ひみつスタジオ",
                     Description = "Secret Studio",
                     Price = 2023,
-                    PictureUrl = "",
+                    PictureUrl = "client/public/images/ひみつスタジオ.jpg",
                     Brand = "Spitz/スピッツ",
                     Type = "Japan Rock",
                     QuantityInStock = 100
@@ -23,9 +23,9 @@ namespace API.Data
                 new Product
                 {
                     Name = "見っけ",
-                    Description = "I found",
+                    Description = "Found it",
                     Price = 2019,
-                    PictureUrl = "",
+                    PictureUrl = "client/public/images/見っけ.jpg",
                     Brand = "Spitz/スピッツ",
                     Type = "Japan Rock",
                     QuantityInStock = 100
@@ -33,9 +33,9 @@ namespace API.Data
                 new Product
                 {
                     Name = "醒めない",
-                    Description = "Can't Wake Up",
+                    Description = "Not Wake Up",
                     Price = 2016,
-                    PictureUrl = "",
+                    PictureUrl = "client/public/images/醒めない.jpg",
                     Brand = "Spitz/スピッツ",
                     Type = "Japan Rock",
                     QuantityInStock = 100
@@ -43,9 +43,19 @@ namespace API.Data
                 new Product
                 {
                     Name = "小さな生き物",
-                    Description = "Small Creature",
+                    Description = "Little Creatures",
                     Price = 2013,
-                    PictureUrl = "",
+                    PictureUrl = "client/public/images/小さな生き物.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "おるたな",
+                    Description = "Orutana",
+                    Price = 2012,
+                    PictureUrl = "client/public/images/おるたな.jpg",
                     Brand = "Spitz/スピッツ",
                     Type = "Japan Rock",
                     QuantityInStock = 100
@@ -55,17 +65,147 @@ namespace API.Data
                     Name = "とげまる",
                     Description = "Spiky Ball",
                     Price = 2010,
-                    PictureUrl = "",
+                    PictureUrl = "client/public/images/とげまる.jpg",
                     Brand = "Spitz/スピッツ",
                     Type = "Japan Rock",
                     QuantityInStock = 100
                 },
                 new Product
                 {
-                    Name = "さざなみ",
-                    Description = "Ripple",
+                    Name = "さざなみ CD",
+                    Description = "Rippl CD",
                     Price = 2007,
-                    PictureUrl = "",
+                    PictureUrl = "client/public/images/さざなみ CD.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "スーベニア",
+                    Description = "Souvenir",
+                    Price = 2005,
+                    PictureUrl = "client/public/images/スーベニア.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "色色衣",
+                    Description = "Various Clothes",
+                    Price = 2004,
+                    PictureUrl = "client/public/images/色色衣.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "三日月ロック",
+                    Description = "Crescent Rock",
+                    Price = 2002,
+                    PictureUrl = "client/public/images/三日月ロック.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "ハヤブサ",
+                    Description = "Falcon",
+                    Price = 2000,
+                    PictureUrl = "client/public/images/ハヤブサ.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "花鳥風月",
+                    Description = "Beauty of Nature",
+                    Price = 1999,
+                    PictureUrl = "client/public/images/花鳥風月.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "フェイクファー",
+                    Description = "Fake Fur",
+                    Price = 1998,
+                    PictureUrl = "client/public/images/フェイクファー.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "インディゴ地平線",
+                    Description = "Indigo Horizon",
+                    Price = 1996,
+                    PictureUrl = "client/public/images/インディゴ地平線.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "ハチミツ",
+                    Description = "Honey",
+                    Price = 1995,
+                    PictureUrl = "client/public/images/ハチミツ.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "空の飛び方",
+                    Description = "How to Fly in the Sky",
+                    Price = 1994,
+                    PictureUrl = "client/public/images/空の飛び方.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Crispy!",
+                    Description = "Crispy!",
+                    Price = 1993,
+                    PictureUrl = "client/public/images/Crispy!.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "惑星のかけら",
+                    Description = "Piece of the Planet",
+                    Price = 1992,
+                    PictureUrl = "client/public/images/惑星のかけら.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "名前をつけてやる",
+                    Description = "I'll Give You a Name",
+                    Price = 1991,
+                    PictureUrl = "client/public/images/名前をつけてやる.jpg",
+                    Brand = "Spitz/スピッツ",
+                    Type = "Japan Rock",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "スピッツ",
+                    Description = "Spitz",
+                    Price = 1991,
+                    PictureUrl = "client/public/images/スピッツ.jpg",
                     Brand = "Spitz/スピッツ",
                     Type = "Japan Rock",
                     QuantityInStock = 100
